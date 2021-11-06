db0829bc10e30415d3ddce88304ef5ae89822937
samo7a <samo7a98@mail.com>
2021-11-06 00:52:58 -0400

    done with the categories

--- lib/components/Categories.dart
+++ lib/components/Categories.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:uber/util/Size.dart';
 
-class Categories extends StatelessWidget {
+class Categories extends StatefulWidget {
   const Categories({Key? key}) : super(key: key);
 
   @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<Map<String, String>> items = [
+    {
+      "image": "assets/images/shopping-bag.png",
+      "text": "Pick-up",
+    },
+    {
+      "image": "assets/images/soft-drink.png",
+      "text": "Soft Drinks",
+    },
+    {
+      "image": "assets/images/bread.png",
+      "text": "Backery Items",
+    },
+    {
+      "image": "assets/images/fast-food.png",
+      "text": "Fast Foods",
+    },
+    {
+      "image": "assets/images/deals.png",
+      "text": "Deals",
+    },
+    {
+      "image": "assets/images/coffee.png",
+      "text": "Coffee & Tea",
+    },
+    {
+      "image": "assets/images/desserts.png",
+      "text": "Desserts",
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Categories"),
+    Size size = Size(context: context);
+    return Padding(
+      padding: EdgeInsets.all(size.BLOCK_WIDTH * 5),
+      child: SizedBox(
+        height: size.BLOCK_HEIGHT * 13,
+        width: size.BLOCK_WIDTH * 90,
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemCount: items.length,
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              width: size.BLOCK_WIDTH * 5,
+            );
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: [
+                SizedBox(
+                  width: size.BLOCK_WIDTH * 12,
+                  height: size.BLOCK_WIDTH * 12,
+                  child: Image.asset(
+                    items[index]["image"] ?? "assets/images/coffee.png",
+                    scale: 1,
+                  ),
+                ),
+                Text(
+                  items[index]["text"] ?? "",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: size.FONT_SIZE * 15,
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
